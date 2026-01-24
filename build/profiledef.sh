bd8e65b7f2ec68f4a2cf61a3c1f5235af4d1dd1a
vegcom <189027736+vegcom@users.noreply.github.com>
2026-01-24 09:01:53 -0600

    fixme/tired don't care fuck me can't be bother fck wrld bro half ass commit

--- build/profiledef.sh
+++ build/profiledef.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
-
 # shellcheck disable=SC2034
+
 iso_name="cozy-arch"
 iso_label="COZY_ARCH"
 iso_publisher="Vegcom"
 iso_application="Cozy Arch Rescue ISO"
 iso_version="2026.01"
+
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('uefi.systemd-boot')
+pacman_conf="pacman.conf"
+
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+
+bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
@@ -17,6 +27,7 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+
   # cozy-files
   ["/etc/issue"]="0:0:644"
   ["/etc/issue.net"]="0:0:644"
